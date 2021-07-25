51e2f80950d29040e9a44db5b518cc529bfc9e6b
sheik <ashok@tmmmt.sa>
2021-07-25 17:10:50 +0530

    voice search basic setup

---
+++ src/Component/voice.js
@@ -0,0 +1,70 @@
+import React from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import { Row, Col, Button } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMicrophone, faUser } from "@fortawesome/free-solid-svg-icons";
+
+const Voice = () => {
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
+  return (
+    <div className="container-fluid">
+      <i class="fas fa-microphone-alt"></i>
+      <Row>
+        <Col md="12" className="text-center">
+          <h2>Voice Search</h2>
+        </Col>
+      </Row>
+      <Row className="mt-2">
+        <Col md="4" className="mr-3 ml-3">
+          <span className="input-group-text">
+            <FontAwesomeIcon
+              icon={faMicrophone}
+              onClick={SpeechRecognition.startListening}
+              style={{
+                marginRight: "10px",
+                color: "red",
+                cursor: "pointer",
+                color: listening ? "red" : "",
+              }}
+            />
+            <span className="text-center"> {transcript}</span>
+          </span>
+        </Col>
+      </Row>
+      <Row className="mt-3">
+        <Col md="4">
+          <Row>
+            <Col md="8"></Col>
+            <Col md="2">
+              <Button
+                className="btn-danger btn-sm"
+                onClick={SpeechRecognition.stopListening}
+              >
+                Stop
+              </Button>
+            </Col>
+            <Col md="2">
+              <Button className="btn-warning btn-sm" onClick={resetTranscript}>
+                Reset
+              </Button>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default Voice;
