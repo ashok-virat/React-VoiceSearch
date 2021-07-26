f43f7d0be96238cd83d96155bf51eb43eec4730d
sheik <ashok@tmmmt.sa>
2021-07-26 10:05:33 +0530

    search button added

--- src/Component/voice.js
+++ src/Component/voice.js
@@ -12,7 +12,8 @@ import {
   Input,
 } from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMicrophone } from "@fortawesome/free-solid-svg-icons";
+import { faMicrophone, faSearch } from "@fortawesome/free-solid-svg-icons";
+import Speech from "react-speech";
 
 const Voice = () => {
   const {
@@ -25,6 +26,66 @@ const Voice = () => {
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser doesn't support speech recognition.</span>;
   }
+
+  const textstyle = {
+    play: {
+      hover: {
+        color: "blue",
+        backgroundColor: "white",
+      },
+      button: {
+        width: "34",
+        height: "34",
+        cursor: "pointer",
+        pointerEvents: "none",
+        outline: "none",
+        backgroundColor: "white",
+        border: "none",
+      },
+    },
+    stop: {
+      hover: {
+        backgroundColor: "GhostWhite",
+      },
+      button: {
+        width: "34",
+        height: "34",
+        cursor: "pointer",
+        pointerEvents: "none",
+        outline: "none",
+        backgroundColor: "red",
+        border: "solid 1px rgba(255,255,255,1)",
+      },
+    },
+    pause: {
+      hover: {
+        backgroundColor: "GhostWhite",
+      },
+      button: {
+        width: "34",
+        height: "34",
+        cursor: "pointer",
+        pointerEvents: "none",
+        outline: "none",
+        backgroundColor: "red",
+        border: "solid 1px rgba(255,255,255,1)",
+      },
+    },
+    resume: {
+      hover: {
+        backgroundColor: "GhostWhite",
+      },
+      button: {
+        width: "34",
+        height: "34",
+        cursor: "pointer",
+        pointerEvents: "none",
+        outline: "none",
+        backgroundColor: "red",
+        border: "solid 1px rgba(255,255,255,1)",
+      },
+    },
+  };
 
   return (
     <div className="container-fluid">
@@ -55,7 +116,27 @@ const Voice = () => {
               placeholder="Voice Search Only..."
               defaultValue={transcript}
               style={{ borderLeft: "none" }}
+              readOnly={true}
             />
+            <InputGroupAddon addonType="append">
+              <InputGroupText
+                style={{
+                  border: "none",
+                  backgroundColor: "white",
+                }}
+              >
+                <Speech
+                  text={transcript}
+                  styles={textstyle}
+                  voice="Google UK English Female"
+                  disabled={transcript.length < 2}
+                  textAsButton={true}
+                  displayText={
+                    <FontAwesomeIcon icon={faSearch}></FontAwesomeIcon>
+                  }
+                ></Speech>
+              </InputGroupText>
+            </InputGroupAddon>
           </InputGroup>
         </Col>
       </Row>
@@ -72,7 +153,14 @@ const Voice = () => {
               </Button>
             </Col>
             <Col md="2" className="mt-2">
-              <Button className="btn-warning btn-sm" onClick={resetTranscript}>
+              <Button
+                className="btn-warning btn-sm"
+                onClick={resetTranscript}
+                style={{
+                  outline: "none",
+                  backgroundColor: "solid 1px rgba(255,255,255,1)",
+                }}
+              >
                 Reset
               </Button>
             </Col>
